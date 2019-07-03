be7f6681f5e69bb6ef3183322fc9d6164e1151bd
vansergen <vansergen@gmail.com>
2019-07-03 12:56:38 -0400

    perf(types): export PublicClient

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,12 @@
-declare module 'gemini-node-api' {}
+declare module 'gemini-node-api' {
+  export type PublicClientOptions = {
+    symbol?: string;
+    sandbox?: boolean;
+    api_uri?: string;
+    timeout?: number;
+  };
+
+  export class PublicClient {
+    constructor(options?: PublicClientOptions);
+  }
+}
