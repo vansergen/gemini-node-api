ffc8c5f2b2a41609e1c846e9fe2f230f51c08f96
vansergen <vansergen@gmail.com>
2019-07-10 19:13:06 -0400

    test(ws): add connectMarket

---
+++ tests/lib/wss.js
@@ -0,0 +1,54 @@
+const wss = require('ws').Server;
+const { SignRequest } = require('../../index.js');
+const querystring = require('querystring');
+
+const messageLimit = 2;
+
+module.exports = ({ port, key, secret }) => {
+  const verifyClient = ({ req }) => {
+    const [, , type, _symbol] = req.url.split('/');
+    const [symbol] = _symbol.split('?');
+    if (type === 'marketdata') {
+      return true;
+    } else if (type === 'order' && symbol === 'events') {
+      const providedSignature = req.headers['x-gemini-signature'];
+      const providedKey = req.headers['x-gemini-apikey'];
+      const providedPayload = req.headers['x-gemini-payload'];
+      const headers = SignRequest({ key, secret }, providedPayload);
+      const requiredSignature = headers['X-GEMINI-SIGNATURE'];
+      return (
+        key &&
+        secret &&
+        providedSignature === requiredSignature &&
+        providedKey === key
+      );
+    }
+    return false;
+  };
+
+  const ws = new wss({ port, verifyClient });
+  ws.on('connection', (socket, req) => {
+    const options = {};
+    const [, , type, path] = req.url.split('/');
+    const [symbol, query] = path.split('?');
+    const qs = querystring.parse(query);
+    if (type === 'marketdata') {
+      for (let key of Object.keys(qs)) {
+        qs[key] = qs[key] === 'true' ? true : false;
+      }
+    }
+    Object.assign(options, qs);
+    for (let i = 0; i < messageLimit; i++) {
+      socket.send(JSON.stringify({ symbol, socket_sequence: i, ...options }));
+    }
+    socket.send(
+      JSON.stringify({
+        symbol,
+        last: true,
+        socket_sequence: messageLimit,
+        ...options,
+      })
+    );
+  });
+  return ws;
+};
--- tests/websocket.spec.js
+++ tests/websocket.spec.js
@@ -1,4 +1,5 @@
 const assert = require('assert');
+const wss = require('./lib/wss.js');
 
 const { WebsocketClient } = require('../index.js');
 
@@ -7,6 +8,9 @@ const {
   DEFAULT_SYMBOL,
   SANDBOX_WS_URL,
 } = require('../lib/utilities.js');
+
+const port = 56255;
+const api_uri = 'ws://localhost:' + port;
 
 const key = 'Gemini-API-key';
 const secret = 'Gemini-API-secret';
@@ -28,5 +32,85 @@ suite('WebsocketClient', () => {
     assert.deepStrictEqual(client.symbol, symbol);
     assert.deepStrictEqual(client.key, key);
     assert.deepStrictEqual(client.secret, secret);
+  });
+
+  test('connectMarket()', done => {
+    const server = wss({ port });
+    const client = new WebsocketClient({ api_uri });
+    client.once('message', (message, _symbol) => {
+      assert.deepStrictEqual(message.socket_sequence, 0);
+      assert.deepStrictEqual(_symbol, symbol);
+      server.close();
+      done();
+    });
+    client.connectMarket({ symbol });
+  });
+
+  test('connectMarket() (with default symbol)', done => {
+    const server = wss({ port });
+    const client = new WebsocketClient({ api_uri, symbol });
+    client.once('message', (message, _symbol) => {
+      assert.deepStrictEqual(message.socket_sequence, 0);
+      assert.deepStrictEqual(_symbol, symbol);
+      server.close();
+      done();
+    });
+    client.connectMarket();
+  });
+
+  test('connectMarket() (with extra parameters)', done => {
+    const heartbeat = true;
+    const top_of_book = true;
+    const bids = true;
+    const offers = true;
+    const trades = false;
+    const auctions = false;
+    const server = wss({ port });
+    const client = new WebsocketClient({ api_uri });
+    client.once('message', (message, _symbol) => {
+      assert.deepStrictEqual(message.socket_sequence, 0);
+      assert.deepStrictEqual(_symbol, DEFAULT_SYMBOL);
+      assert.deepStrictEqual(message.heartbeat, heartbeat);
+      assert.deepStrictEqual(message.top_of_book, top_of_book);
+      assert.deepStrictEqual(message.bids, bids);
+      assert.deepStrictEqual(message.offers, offers);
+      assert.deepStrictEqual(message.trades, trades);
+      assert.deepStrictEqual(message.auctions, auctions);
+      server.close();
+      done();
+    });
+    client.connectMarket({
+      heartbeat,
+      top_of_book,
+      bids,
+      offers,
+      trades,
+      auctions,
+    });
+  });
+
+  test('connectMarket() (connects to different markets)', done => {
+    const server = wss({ port });
+    const client = new WebsocketClient({ api_uri });
+    const last = { [DEFAULT_SYMBOL]: false, [symbol]: false };
+    const seq = { [DEFAULT_SYMBOL]: 0, [symbol]: 0 };
+    client.on('message', (message, _symbol) => {
+      if (_symbol === DEFAULT_SYMBOL) {
+        assert.deepStrictEqual(message.socket_sequence, seq[DEFAULT_SYMBOL]++);
+      } else if (_symbol === symbol) {
+        assert.deepStrictEqual(message.socket_sequence, seq[symbol]++);
+      } else {
+        assert.fail('Unrecognized symbol');
+      }
+      if (message.last) {
+        last[_symbol] = true;
+        if (last[symbol] && last[DEFAULT_SYMBOL]) {
+          server.close();
+          done();
+        }
+      }
+    });
+    client.connectMarket({ symbol });
+    client.connectMarket();
   });
 });
