1539e4be55645ba924d4f91c116881d4ba790d99
vansergen <vansergen@gmail.com>
2019-07-10 17:51:34 -0400

    feat(module): add WebsocketClient

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ module.exports = {
   AuthenticatedClient: require('./lib/authenticated.js'),
   PublicClient: require('./lib/public.js'),
   SignRequest: require('./lib/signer.js'),
+  WebsocketClient: require('./lib/websocket.js'),
 };
