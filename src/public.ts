a9b20dadbc5825c02c2305462f4ce6208bdfba68
Sergey Bakulin <vansergen@gmail.com>
2019-11-29 12:43:20 -0500

    feat(public): export default constants

---
+++ src/public.ts
@@ -0,0 +1,13 @@
+export const ApiLimit = 500;
+export const DefaultSymbol = "btcusd";
+export const DefaulTimeout = 10000;
+export const ApiUrl = "https://api.gemini.com";
+export const SandboxApiUrl = "https://api.sandbox.gemini.com";
+export const Headers = {
+  "User-Agent": "gemini-node-api-client",
+  "Content-Type": "application/json",
+  Accept: "application/json",
+  "X-Requested-With": "XMLHttpRequest",
+  "Content-Length": 0,
+  "Cache-Control": "no-cache"
+};
