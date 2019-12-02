de15b46728ba7604f6c73e352ad255446d25ddad
Sergey Bakulin <vansergen@gmail.com>
2019-12-02 12:27:24 -0500

    test(auth): add createAccount

---
+++ test/auth.spec.ts
@@ -0,0 +1,59 @@
+import * as assert from "assert";
+import * as nock from "nock";
+import {
+  AuthenticatedClient,
+  Headers,
+  ApiUri,
+  SignRequest,
+  Account
+} from "../index";
+
+const key = "Gemini-API-KEY";
+const secret = "Gemini-API-SECRET";
+
+const client = new AuthenticatedClient({ key, secret });
+const nonce = Date.now();
+const _nonce = () => nonce;
+client.nonce = _nonce;
+
+suite("AuthenticatedClient", () => {
+  test("constructor", () => {
+    const sandbox = true;
+    const apiUri = "https://new-gemini-api-uri.com";
+    const timeout = 9000;
+    const symbol = "zecbtc";
+    const client = new AuthenticatedClient({
+      sandbox,
+      apiUri,
+      timeout,
+      symbol,
+      key,
+      secret
+    });
+    client.nonce = _nonce;
+    assert.deepStrictEqual(client._rpoptions, {
+      timeout,
+      baseUrl: apiUri,
+      json: true,
+      headers: Headers
+    });
+    assert.deepStrictEqual(client.symbol, symbol);
+    assert.deepStrictEqual(client.key, key);
+    assert.deepStrictEqual(client.secret, secret);
+    assert.deepStrictEqual(client.nonce, _nonce);
+  });
+
+  test(".createAccount()", async () => {
+    const request = "/v1/account/create";
+    const name = "name";
+    const type = "custody";
+    const options = { request, name, type, nonce };
+    const response: Account = { name, type };
+    nock(ApiUri, { reqheaders: { ...SignRequest({ key, secret, options }) } })
+      .post(request, {})
+      .reply(200, response);
+
+    const data = await client.createAccount({ name, type });
+    assert.deepStrictEqual(data, response);
+  });
+});
