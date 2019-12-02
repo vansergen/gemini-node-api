785dad6e1d5e7598e8b08089bafc5965e5fb2f5f
Sergey Bakulin <vansergen@gmail.com>
2019-12-02 12:26:35 -0500

    feat(auth): add createAccount method

--- src/auth.ts
+++ src/auth.ts
@@ -3,6 +3,8 @@ import { RequestPromise as Promise } from "request-promise";
 import { PublicClient, PublicClientOptions, Headers } from "./public";
 import { SignRequest } from "./signer";
 
+export type Account = { name: string; type?: "exchange" | "custody" };
+
 export type AuthenticatedClientOptions = PublicClientOptions & {
   key: string;
   secret: string;
@@ -25,6 +27,13 @@ export class AuthenticatedClient extends PublicClient {
     const request = { key: this.key, secret: this.secret, options: body };
     const headers = { ...Headers, ...SignRequest(request) };
     return super.post({ uri, headers });
+  }
+
+  /**
+   * Create a new exchange account within the group.
+   */
+  createAccount(body: Account): Promise<Account> {
+    return this.post({ body: { request: "/v1/account/create", ...body } });
   }
 
   get nonce(): () => number {
